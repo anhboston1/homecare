ff7c0c5ee400859e9b591b6064425c566fc5b8cd
Phuong Nguyen <anhdinhphuong@hotmail.com>
2022-10-26 12:12:04 -0400

    4/26

--- src/app/homeowner/postproject/postproject.page.ts
+++ src/app/homeowner/postproject/postproject.page.ts
@@ -1,16 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-postproject',
   templateUrl: './postproject.page.html',
   styleUrls: ['./postproject.page.scss'],
 })
+
 export class PostprojectPage implements OnInit {
   mode = "new"; //"new", "edit", "view"
+  project:Object = {
+    "projectid": "2", 
+    "title": "Replace toilet", 
+    "description": "Paint ceiling", 
+    "posetDate": "10/24/2022", 
+    "priceRange": "",
+    "address": {},
+    "ownerId": "",
+    "projectPhotos": {},
+    "status": "",
+    "tags": [],
+    "category": "",
+    "subCategory": "",
+    "bids": [{"bidderId": "", "bidDate": "", "price": "", "proposal": "", "startDate": "", "completeDate": ""}],
+   } 
 
-  constructor() { }
+   projectForm: FormGroup;
+   get title() { return this.projectForm.get('title'); }
+   get description() { return this.projectForm.get('description'); }
+   get category() { return this.projectForm.get('category'); }
+  constructor(private fb: FormBuilder) { 
+    this.projectForm = this.fb.group({
+      title: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      description: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      category: [''],
+    }, {
+      validators: []
+    });
+  }
 
   ngOnInit() {
   }
-
+  onSubmitTemplateBased() {
+    //console.log(this.project);
+  }
 }
--- src/app/homeowner/projectlist/projectlist.page.ts
+++ src/app/homeowner/projectlist/projectlist.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserprofileService } from '../../services/userprofile.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-projectlist',
@@ -8,17 +9,21 @@ import { UserprofileService } from '../../services/userprofile.service';
 })
 export class ProjectlistPage implements OnInit {
 
-  constructor(private uService: UserprofileService) { }
+  constructor(private uService: UserprofileService, private router: Router) { }
 
   ngOnInit() {
   }
 
   viewProject(id) {
+    this.router.navigateByUrl('/postproject');
   }
 
   editProject(id) {
   }
-  
+
   deleteProject(id) {
+  }
+  add(){
+    this.router.navigateByUrl('/postproject');
   }
 }
